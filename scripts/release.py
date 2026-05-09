8cace909651e2606094f85e0cd14a71f9587fdcd
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-09 14:50:53 -0700

    chore: add ming1523 to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -64,6 +64,7 @@ AUTHOR_MAP = {
     "ytchen0719@gmail.com": "liquidchen",
     "am@studio1.tailb672fe.ts.net": "subtract0",
     "axmaiqiu@gmail.com": "qWaitCrypto",
+    "silverchris@foxmail.com": "ming1523",
     "maksesipov@gmail.com": "Qwinty",
     "denisamania@gmail.com": "CalmProton",
     "308068+mbac@users.noreply.github.com": "mbac",
